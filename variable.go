fc87f7e6d33ce06e66ea78141e40eecdf6938826
vikrantdhawan <vikrant.dhawan@gojek.com>
2024-05-05 02:09:47 +0530

    updating import

--- variable.go
+++ variable.go
@@ -1,6 +1,6 @@
 package gr_variable
 
-import "gr-variable/chan_variable"
+import "github.com/vd09/gr-variable/chan_variable"
 
 func NewGrChannel[T any]() GrChannel[T] {
 	return chan_variable.NewCharVar[T]()
